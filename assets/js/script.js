7fa832605bbe40bcf0b96e745a4d73b227e7b12a
leoniemclaughlin7 <>
2023-04-30 21:31:05 +0000

    Add movePiece function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+
 let roll = document.getElementById('roll');
 roll.addEventListener('click', rollDice);
+roll.addEventListener('click', movePiece);
 
 function rollDice() {
     let randomNum = Math.floor(Math.random() * 6) + 1;
@@ -9,20 +11,52 @@ function rollDice() {
 
     if (randomNum === 1) {
         document.getElementById("img").src = "assets/images/dice1.jfif";
-    }
-    if (randomNum === 2) {
+    } else if (randomNum === 2) {
         document.getElementById("img").src = "assets/images/dice2.jfif";
-    }
-    if (randomNum === 3) {
+    } else if (randomNum === 3) {
         document.getElementById("img").src = "assets/images/dice3.jfif";
-    }
-    if (randomNum === 4) {
+    } else if (randomNum === 4) {
         document.getElementById("img").src = "assets/images/dice4.jfif";
-    }
-    if (randomNum === 5) {
+    } else if (randomNum === 5) {
         document.getElementById("img").src = "assets/images/dice5.jfif";
-    }
-    if (randomNum === 6) {
+    } else {
         document.getElementById("img").src = "assets/images/dice6.jfif";
     }
+
+    return randomNum;
+
 }
+
+let pieceOne = document.getElementById('piece-one');
+let squares = document.getElementsByClassName('board-square');
+let currentPosition = 0;
+const board = [
+    'recharge',
+    'city-garden',
+    'earthship',
+    'hydroponics',
+    'ev-car',
+    'greenway',
+    'cycle-path',
+    'greenspace',
+    'hydroelectric',
+    'solar-farm',
+    'community-work',
+    'habitat-survey'
+];
+
+function movePiece() {
+    let diceRoll = rollDice();
+    currentPosition += diceRoll;
+    if (currentPosition >= board.length) {
+        currentPosition -= board.length;
+    }
+    let currentSquare = squares[currentPosition];
+    currentSquare.appendChild(pieceOne);
+    console.log(pieceOne);
+
+}
+
+
+
+
